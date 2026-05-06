c6a5db4cae1ac8da77b9129245646c5bccd5de28
Iryna Kaida <kaidairyna@gmail.com>
2026-05-06 14:56:50 +0300

    implement calculateRentalCost using formula instead of hardcoded values

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentday = 40;
+
+  if (days < 3) {
+    return rentday * days;
+  } else if (days <= 6) {
+    return rentday * days - 20;
+  } else if (days >= 7) {
+    return rentday * days - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
